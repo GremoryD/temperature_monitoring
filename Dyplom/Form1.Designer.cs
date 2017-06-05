ec4ae280021f2147fdf0f76f7e54175248011f65
GremoryD <dima.lamashevski@gmail.com>
2017-06-06 00:50:34 +0300

    First
    First repositori of Dyplom project

---
+++ Dyplom/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Dyplom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(607, 416);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel3);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(599, 390);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Процессор";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.listView1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 28);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(593, 359);
+            this.panel3.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(593, 359);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(593, 25);
+            this.panel1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Убрать значения null";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(402, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(599, 390);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "График температуры CPU ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(593, 384);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.chart2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(599, 390);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "График температуры GPU";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(0, 0);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(599, 390);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.numericUpDown1);
+            this.tabPage4.Controls.Add(this.label1);
+            this.tabPage4.Controls.Add(this.button2);
+            this.tabPage4.Controls.Add(this.button1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(599, 390);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Настроики";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(240, 15);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Интервал таймера";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(354, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Сохранить данные Температуры GPU";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(354, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сохранить данные Температуры CPU";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 416);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Dyplom/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Windows.Forms.DataVisualization.Charting; 
+using OpenHardwareMonitor;
+using OpenHardwareMonitor.Collections;
+using OpenHardwareMonitor.Hardware;
+using System.IO;
+using System.Diagnostics; 
+
+namespace Dyplom
+{
+   
+    public partial class Form1 : Form
+    { 
+        struct Data
+        {
+           public Point point;
+           public DateTime time;
+        }
+        List<Data> CPUdata;
+        List<Data> GPUdata;
+
+
+
+        Series mySeriesOfPoint;
+        Series mySeriesOfPoint2;
+        private DateTime now;
+        protected readonly ListSet<ISensor> active = new ListSet<ISensor>();
+        public event SensorEventHandler SensorAdded;
+        public event SensorEventHandler SensorRemoved;
+
+        protected virtual void ActivateSensor(ISensor sensor)
+        {
+            if (active.Add(sensor))
+                if (SensorAdded != null)
+                    SensorAdded(sensor);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CPUdata = new List<Data>();
+            GPUdata = new List<Data>();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(new object[] {
+            "Win32_1394Controller",
+            "Win32_1394ControllerDevice",
+            "Win32_BaseBoard",
+            "Win32_Battery",
+            "Win32_BIOS",
+            "Win32_Bus",
+            "Win32_CDROMDrive",
+            "Win32_CIMLogicalDeviceCIMDataFile",
+            "Win32_DeviceBus",
+            "Win32_DeviceMemoryAddress",
+            "Win32_DeviceSettings",
+            "Win32_DisplayConfiguration",
+            "Win32_DisplayControllerConfiguration",
+            "Win32_DMAChannel",
+            "Win32_DriverVXD",
+            "Win32_FloppyController",
+            "Win32_FloppyDrive",
+            "Win32_HeatPipe",
+            "Win32_IDEController",
+            "Win32_IDEControllerDevice",
+            "Win32_InfraredDevice",
+            "Win32_IRQResource",
+            "Win32_Keyboard",
+            "Win32_MotherboardDevice",
+            "Win32_OnBoardDevice",
+            "Win32_PCMCIAController",
+            "Win32_PNPAllocatedResource",
+            "Win32_PnPDevice",
+            "Win32_PnPEntity",
+            "Win32_PointingDevice",
+            "Win32_PortableBattery",
+            "Win32_PortConnector",
+            "Win32_PortResource",
+            "Win32_POTSModem",
+            "Win32_POTSModemToSerialPort",
+            "Win32_PowerManagementEvent",
+            "Win32_Printer",
+            "Win32_PrinterConfiguration",
+            "Win32_PrinterController",
+            "Win32_PrinterDriverDll",
+            "Win32_PrinterSetting",
+            "Win32_PrinterShare",
+            "Win32_PrintJob",
+            "Win32_Processor",
+            "Win32_SCSIController",
+            "Win32_SCSIControllerDevice",
+            "Win32_SerialPort",
+            "Win32_SerialPortConfiguration",
+            "Win32_SerialPortSetting",
+            "Win32_SMBIOSMemory",
+            "Win32_SoundDevice",
+            "Win32_TemperatureProbe",
+            "Win32_USBController",
+            "Win32_USBControllerDevice",
+            "Win32_VideoConfiguration",
+            "Win32_VideoController",
+            "Win32_VideoSettings",
+            "Win32_VoltageProbe"});
+            comboBox1.SelectedItem = "Win32_Processor";
+            ColumnHeader columnHeader1 = new ColumnHeader();
+            ColumnHeader columnHeader2 = new ColumnHeader();
+            columnHeader1.Text = "Название";
+            columnHeader2.Text = "Значение";
+            listView1.Columns.AddRange(new ColumnHeader[] {
+            columnHeader1,
+            columnHeader2});
+
+            chart1.ChartAreas.Add(new ChartArea("Math functions"));
+            mySeriesOfPoint = new Series("Temprecher CPU");
+            mySeriesOfPoint.ChartType = SeriesChartType.Line;
+            mySeriesOfPoint2 = new Series("Temprecher GPU");
+            mySeriesOfPoint2.ChartType = SeriesChartType.Line;
+
+            chart1.Series.Add(mySeriesOfPoint);
+            chart2.Series.Add(mySeriesOfPoint2);
+
+
+
+
+        }
+
+        private void InsertInfo(string Key, ref ListView lst, bool DontInsertNull)
+        {
+            lst.Items.Clear();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from " + Key);
+
+            try
+            {
+                foreach (ManagementObject share in searcher.Get())
+                {
+
+                    ListViewGroup grp;
+                    try
+                    {
+                        grp = lst.Groups.Add(share["Name"].ToString(), share["Name"].ToString());
+                    }
+                    catch
+                    {
+                        grp = lst.Groups.Add(share.ToString(), share.ToString());
+                    }
+
+                    if (share.Properties.Count <= 0)
+                    {
+                        MessageBox.Show("No Information Available", "No Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                    foreach (PropertyData PC in share.Properties)
+                    {
+
+                        ListViewItem item = new ListViewItem(grp);
+                        if (lst.Items.Count % 2 != 0)
+                            item.BackColor = Color.White;
+                        else
+                            item.BackColor = Color.WhiteSmoke;
+
+                        item.Text = PC.Name;
+
+                        if (PC.Value != null && PC.Value.ToString() != "")
+                        {
+                            switch (PC.Value.GetType().ToString())
+                            {
+                                case "System.String[]":
+                                    string[] str = (string[])PC.Value;
+
+                                    string str2 = "";
+                                    foreach (string st in str)
+                                        str2 += st + " ";
+
+                                    item.SubItems.Add(str2);
+
+                                    break;
+                                case "System.UInt16[]":
+                                    ushort[] shortData = (ushort[])PC.Value;
+
+
+                                    string tstr2 = "";
+                                    foreach (ushort st in shortData)
+                                        tstr2 += st.ToString() + " ";
+
+                                    item.SubItems.Add(tstr2);
+
+                                    break;
+
+                                default:
+                                    item.SubItems.Add(PC.Value.ToString());
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            if (!DontInsertNull)
+                                item.SubItems.Add("No Information available");
+                            else
+                                continue;
+                        }
+                        lst.Items.Add(item);
+                    }
+                }
+            }
+
+
+            catch (Exception exp)
+            {
+                MessageBox.Show("can't get data because of the followeing error \n" + exp.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+        }
+
+        private void RemoveNullValue(ref ListView lst)
+        {
+            foreach (ListViewItem item in lst.Items)
+                if (item.SubItems[1].Text == "No Information available")
+                    item.Remove();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                RemoveNullValue(ref listView1);
+            else
+                InsertInfo(comboBox1.SelectedItem.ToString(), ref listView1, checkBox1.Checked);
+        }
+        int i = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                List<byte> ls = GetDriveTemp();
+                int temperature1 = Convert.ToInt32(ls[0]);
+                int temperature2 = Convert.ToInt32(ls[1]);
+                mySeriesOfPoint.Points.AddXY(i, temperature1);
+                mySeriesOfPoint2.Points.AddXY(i, temperature2);
+                i += (int)numericUpDown1.Value;
+
+                Data temp = new Data();
+                temp.time = DateTime.Now;
+                temp.point =  new Point(i, temperature1);
+                CPUdata.Add(temp);
+                temp = new Data();
+                temp.time = DateTime.Now;
+                temp.point = new Point(i, temperature2);
+                GPUdata.Add(temp);
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+                timer1.Stop(); 
+            }
+        } 
+
+        const byte TEMPERATURE_HDD = 194;
+        static public List<byte> GetDriveTemp()
+        {
+            List<byte> retval = new List<byte>();
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\WMI", "SELECT * FROM MSStorageDriver_ATAPISmartData");
+                //loop through all the hard disks
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    byte[] arrVendorSpecific = (byte[])queryObj.GetPropertyValue("VendorSpecific");
+                    //Find the temperature attribute
+                    int tempIndex = Array.IndexOf(arrVendorSpecific, TEMPERATURE_HDD);
+                    retval.Add(arrVendorSpecific[tempIndex + 5]);
+                }
+            }
+            catch (ManagementException err)
+            {
+                Console.WriteLine("An error occurred while querying for WMI data: " + err.Message);
+            }
+            return retval;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)numericUpDown1.Value*1000;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+      
+
+}
